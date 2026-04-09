2b0e67c0b7bacb4b02515880e193a33461f5681f
wiut00016734 <wiut00016734@gmail.com>
2026-04-09 21:28:24 +0500

    changed domain url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // ── Middleware ──
-app.use(cors());
+app.use(cors({ origin: 'pandora-production-0ca5.up.railway.app' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
